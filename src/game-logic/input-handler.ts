ef53d58d94ce1a319afecb94adac10acd41bfd37
Esa Koskinen <esakoskin2@gmail.com>
2020-09-26 21:41:59 +0900

    reformat input handler

--- src/game-logic/input-handler.ts
+++ src/game-logic/input-handler.ts
@@ -1,13 +1,37 @@
-import * as kiltagear from "../kiltagear";
-import { Player, KeyStatus, InputStatus, InGameState, CharacterSelectionState, ActiveAttack, AttackStrength, AttackDirection, CharacterState, Attack, PlayerInput } from "../types";
-import { getAttackString, playerCanAct, playerCanSDI, isDirectionalInput } from "../utilities";
-import { handlePlayerMove, handlePlayerJump, handlePlayerFastFall, handlePlayerSDI } from "./physics";
+import * as kiltagear from '../kiltagear'
+import {
+  Player,
+  KeyStatus,
+  InputStatus,
+  InGameState,
+  CharacterSelectionState,
+  ActiveAttack,
+  AttackStrength,
+  AttackDirection,
+  CharacterState,
+  Attack,
+  PlayerInput,
+} from '../types'
+import { getAttackString, playerCanAct, playerCanSDI, isDirectionalInput } from '../utilities'
+import {
+  handlePlayerMove,
+  handlePlayerJump,
+  handlePlayerFastFall,
+  handlePlayerSDI,
+} from './physics'
 
 function inputHeld(player: Player, inputs: InputStatus, inputToCheck: PlayerInput) {
-  return player.playerInputs[inputToCheck].some(keyName => inputs[keyName] && inputs[keyName].isDown)
+  return player.playerInputs[inputToCheck].some(
+    (keyName) => inputs[keyName] && inputs[keyName].isDown
+  )
 }
 
-export const handlePlayerInputs = (currentState: InGameState, inputs: InputStatus, keysPressed: KeyStatus[], keysReleased: KeyStatus[]): InGameState => {
+export const handlePlayerInputs = (
+  currentState: InGameState,
+  inputs: InputStatus,
+  keysPressed: KeyStatus[],
+  keysReleased: KeyStatus[]
+): InGameState => {
   const nextState: InGameState = { ...currentState }
   const nextPlayers: Player[] = nextState.players.slice() // shallow copy
 
@@ -15,10 +39,16 @@ export const handlePlayerInputs = (currentState: InGameState, inputs: InputStatu
     const inputMap: { [key: string]: PlayerInput } = kiltagear.inputMaps[player.playerSlot]
 
     // Check held inputs (i.e. horizontal movement)
-    if (inputHeld(player, inputs, PlayerInput.Right) && !inputHeld(player, inputs, PlayerInput.Left)) {
+    if (
+      inputHeld(player, inputs, PlayerInput.Right) &&
+      !inputHeld(player, inputs, PlayerInput.Left)
+    ) {
       player = handlePlayerMove(player, 1, currentState)
     }
-    if (inputHeld(player, inputs, PlayerInput.Left) && !inputHeld(player, inputs, PlayerInput.Right)) {
+    if (
+      inputHeld(player, inputs, PlayerInput.Left) &&
+      !inputHeld(player, inputs, PlayerInput.Right)
+    ) {
       player = handlePlayerMove(player, -1, currentState)
     }
 
@@ -43,7 +73,12 @@ export const handlePlayerInputs = (currentState: InGameState, inputs: InputStatu
             break
 
           case PlayerInput.Special:
-            nextState.activeAttacks = handleAttack('Special', player, inputs, nextState.activeAttacks)
+            nextState.activeAttacks = handleAttack(
+              'Special',
+              player,
+              inputs,
+              nextState.activeAttacks
+            )
             break
 
           case PlayerInput.Meter:
@@ -62,7 +97,12 @@ export const handlePlayerInputs = (currentState: InGameState, inputs: InputStatu
 }
 
 // Mutate passed player and state to add a new attack based on the input
-function handleAttack(inputName: AttackStrength, player: Player, inputs: InputStatus, activeAttacks: ActiveAttack[]): ActiveAttack[] {
+function handleAttack(
+  inputName: AttackStrength,
+  player: Player,
+  inputs: InputStatus,
+  activeAttacks: ActiveAttack[]
+): ActiveAttack[] {
   if (playerCanAct(player)) {
     const attackDirection = getAttackDirection(player, inputs)
     const attack: Attack | undefined = getCharacterAttack(inputName, player, attackDirection)
@@ -77,7 +117,11 @@ function handleAttack(inputName: AttackStrength, player: Player, inputs: InputSt
   return activeAttacks
 }
 
-function getCharacterAttack(attackStrength: AttackStrength, player: Player, attackDirection: AttackDirection): Attack | undefined {
+function getCharacterAttack(
+  attackStrength: AttackStrength,
+  player: Player,
+  attackDirection: AttackDirection
+): Attack | undefined {
   if (playerCanAct(player)) {
     const attackId = getAttackString(player, attackStrength, attackDirection)
     const attack: Attack | undefined = player.character.attacks[attackId]
@@ -92,6 +136,7 @@ function getCharacterAttack(attackStrength: AttackStrength, player: Player, atta
   return undefined
 }
 
+// prettier-ignore
 function getAttackDirection(player: Player, inputs: InputStatus): AttackDirection {
   const isHoldingLeft =  (inputHeld(player, inputs, PlayerInput.Left))
   const isHoldingRight = (inputHeld(player, inputs, PlayerInput.Right))
@@ -107,42 +152,53 @@ function getAttackDirection(player: Player, inputs: InputStatus): AttackDirectio
   return attackDirection
 }
 
-function inputToAttackDirection(input: PlayerInput, facing: 'left' | 'right', state: CharacterState): AttackDirection {
+function inputToAttackDirection(
+  input: PlayerInput,
+  facing: 'left' | 'right',
+  state: CharacterState
+): AttackDirection {
   switch (input) {
     // Keep facing while airborne, eliminate illegal directions based on state
-    case PlayerInput.Left: return state === 'groundborne' ? 'Forward' : (facing === 'left' ? 'Forward' : 'Back')
-    case PlayerInput.Right: return state === 'groundborne' ? 'Forward' : (facing === 'right' ? 'Forward' : 'Back')
-    case PlayerInput.Down: return 'Down'
-    case PlayerInput.Up: return state === 'airborne' ? 'Up' : 'Neutral'
-    case PlayerInput.Neutral: return 'Neutral'
-    case undefined: return 'Neutral'
+    case PlayerInput.Left:
+      return state === 'groundborne' ? 'Forward' : facing === 'left' ? 'Forward' : 'Back'
+    case PlayerInput.Right:
+      return state === 'groundborne' ? 'Forward' : facing === 'right' ? 'Forward' : 'Back'
+    case PlayerInput.Down:
+      return 'Down'
+    case PlayerInput.Up:
+      return state === 'airborne' ? 'Up' : 'Neutral'
+    case PlayerInput.Neutral:
+      return 'Neutral'
+    case undefined:
+      return 'Neutral'
     default:
       return 'Neutral'
   }
 }
 
-function addActiveAttack(attack: Attack, activeAttacks: ActiveAttack[], player: Player, attackDirection: AttackDirection): ActiveAttack[] {
+function addActiveAttack(
+  attack: Attack,
+  activeAttacks: ActiveAttack[],
+  player: Player,
+  attackDirection: AttackDirection
+): ActiveAttack[] {
   // Handle all the nasty mirroring from facing left, doing an airBack move, etc here, so other parts of the game don't have to
   const xDirection = player.facing === 'left' ? -1 : 1
   const xMultiplierOnHit = attackDirection === 'Back' ? -1 : 1
   const xSpeed = xDirection * attack.xSpeed
 
-  const attackX = attack.createUsingWorldCoordinates || attack.movesWithPlayer
+  const attackX =
+    attack.createUsingWorldCoordinates || attack.movesWithPlayer
       ? attack.x
-      : player.x + (attack.x * xDirection)
-  const attackY = attack.createUsingWorldCoordinates || attack.movesWithPlayer
-      ? attack.y
-      : player.y + attack.y
+      : player.x + attack.x * xDirection
+  const attackY =
+    attack.createUsingWorldCoordinates || attack.movesWithPlayer ? attack.y : player.y + attack.y
 
-  const newHitboxes = attack.hitboxes
-      .slice()
-      .map(hitbox =>
-        ({
-          ...hitbox,
-          x: hitbox.x * xDirection,
-          knockbackX: hitbox.knockbackX * xDirection * xMultiplierOnHit,
-        })
-      )
+  const newHitboxes = attack.hitboxes.slice().map((hitbox) => ({
+    ...hitbox,
+    x: hitbox.x * xDirection,
+    knockbackX: hitbox.knockbackX * xDirection * xMultiplierOnHit,
+  }))
 
   const newActiveAttack: ActiveAttack = {
     ...attack,
@@ -151,13 +207,16 @@ function addActiveAttack(attack: Attack, activeAttacks: ActiveAttack[], player: 
     y: attackY,
     xSpeed,
     hitboxes: newHitboxes,
-    currentFrame: 0
+    currentFrame: 0,
   }
 
   return activeAttacks.concat(newActiveAttack)
 }
 
-export const handleCharacterSelection = (currentState: CharacterSelectionState, keysPressed: KeyStatus[]): CharacterSelectionState => {
+export const handleCharacterSelection = (
+  currentState: CharacterSelectionState,
+  keysPressed: KeyStatus[]
+): CharacterSelectionState => {
   const nextState: CharacterSelectionState = currentState
   const lastCharacterIndex: number = kiltagear.characters.length - 1
 
@@ -187,8 +246,7 @@ export const handleCharacterSelection = (currentState: CharacterSelectionState, 
         }
         break
       case 'v':
-        if (!nextState.start)
-          nextState.playerReady[0] = false
+        if (!nextState.start) nextState.playerReady[0] = false
         break
       case 'ArrowUp':
         if (!currentState.playerReady[1] && currentState.characterSelection[1] > 0)
@@ -214,8 +272,7 @@ export const handleCharacterSelection = (currentState: CharacterSelectionState, 
         }
         break
       case '.':
-        if (!nextState.start)
-          nextState.playerReady[1] = false
+        if (!nextState.start) nextState.playerReady[1] = false
         break
     }
   })
