ff8d62638f345b3ba62683acf2cfac06b1e834ea
Esa Koskinen <esakoskin2@gmail.com>
2020-09-27 01:51:34 +0900

    reindent the character files

--- src/characters/katshuma.ts
+++ src/characters/katshuma.ts
@@ -4,280 +4,280 @@ import { generateAttack, createHitbox, gainMeter, sum } from "../utilities";
 const METER_GAIN_MULTIPLIER = 0.055
 
 export const Katshuma: Character = {
-    name: 'Katshuma',
-    id: 'katshuma',
-    maxHealth: 100,
-    maxMeter: 100,
-    startingMeter: 0,
-    meterThresholds: [25, 50, 75, 100], // [10, 20, ..., 100]
-    walkSpeed: 8.5,
-    airSpeed: 10,
-    weight: 1,
-    maxJumps: 2,
-    jumpStrength: 1,
-    hurtboxRadius: 20,
-    onEachFrame:
-        // Return a meter-gain function that keeps state in a closure
-        (() => {
-            let xDiff = 99999999
-            return (player: Player, previousState: InGameState) => {
+  name: 'Katshuma',
+  id: 'katshuma',
+  maxHealth: 100,
+  maxMeter: 100,
+  startingMeter: 0,
+  meterThresholds: [25, 50, 75, 100], // [10, 20, ..., 100]
+  walkSpeed: 8.5,
+  airSpeed: 10,
+  weight: 1,
+  maxJumps: 2,
+  jumpStrength: 1,
+  hurtboxRadius: 20,
+  onEachFrame:
+  // Return a meter-gain function that keeps state in a closure
+  (() => {
+    let xDiff = 99999999
+    return (player: Player, previousState: InGameState) => {
 
-                // Sum of the x-differences between Katshuma and each other player
-                const otherPlayers = previousState.players.filter(otherPlayer => otherPlayer.playerSlot !== player.playerSlot)
-                const newXDiff = sum(otherPlayers.map(op => Math.abs(op.x - player.x)))
+      // Sum of the x-differences between Katshuma and each other player
+      const otherPlayers = previousState.players.filter(otherPlayer => otherPlayer.playerSlot !== player.playerSlot)
+      const newXDiff = sum(otherPlayers.map(op => Math.abs(op.x - player.x)))
 
-                // If distance increased, gain meter in relation to the change in the sum of x-differences, divided with the number of players
-                const meterGain = Math.max(0, (newXDiff - xDiff) / otherPlayers.length * METER_GAIN_MULTIPLIER)
-                xDiff = newXDiff
-                return gainMeter(meterGain, player)
-            }
-        })(),
-    onMove: (player: Player) => player,
-    onJump: (player: Player) => player,
-    onAttackHit: (player: Player) => player,
-    onGetHit: (player: Player) => player,
-    attacks: {
-        LightNeutral: {
-            ...generateAttack([
-                { ...createHitbox(4, 12, 5), radius: 123 },
-                { ...createHitbox(12, 20, 10), hitLag: 100 }
-            ]),
-            endWhenHitboxConnects: false
-        },
-        LightForward: {
-            ...generateAttack([
-                {
-                    damage: 4,
-                    radius: 20,
-                    knockbackBase: 6,
-                    knockbackGrowth: 2,
-                    knockbackX: 0.5,
-                    knockbackY: 1,
-                    hitstunBase: 20,
-                    hitstunGrowth: 1,
-                    hitLag: 12,
-                    ignoreOwnerHitlag: false,
-                    // characterSpecific: ,
-                    x: 5,
-                    y: 5,
-                    framesUntilActivation: 0,
-                    duration: 40,
-                    onActivation: (state) => { return state },
-                    onHit: (state) => { return state },
-                    onEnd: (state) => { return state },
-                }
-            ])
-        },
-        LightDown: {
-            ...generateAttack([
-                {
-                    damage: 6,
-                    radius: 20,
-                    knockbackBase: 15,
-                    knockbackGrowth: 5,
-                    knockbackX: 1,
-                    knockbackY: -1,
-                    hitstunBase: 30,
-                    hitstunGrowth: 1,
-                    hitLag: 12,
-                    ignoreOwnerHitlag: false,
-                    // characterSpecific: ,
-                    x: 30,
-                    y: 30,
-                    framesUntilActivation: 0,
-                    duration: 40,
-                    onActivation: (state) => { return state },
-                    onHit: (state) => { return state },
-                    onEnd: (state) => { return state },
-                }
-            ])
-        },
-        airLightNeutral: {
-            ...generateAttack([
-                {
-                    damage: 9,
-                    radius: 20,
-                    knockbackBase: 1,
-                    knockbackGrowth: 1,
-                    knockbackX: 1,
-                    knockbackY: 1,
-                    hitstunBase: 20,
-                    hitstunGrowth: 1,
-                    hitLag: 12,
-                    ignoreOwnerHitlag: false,
-                    // characterSpecific: ,
-                    x: 0,
-                    y: -5,
-                    framesUntilActivation: 0,
-                    duration: 40,
-                    onActivation: (state) => { return state },
-                    onHit: (state) => { return state },
-                    onEnd: (state) => { return state },
-                }
-            ])
-        },
-        airLightUp: {
-            ...generateAttack([
-                {
-                    damage: 4,
-                    radius: 20,
-                    knockbackBase: 12,
-                    knockbackGrowth: 2,
-                    knockbackX: 0,
-                    knockbackY: 1,
-                    hitstunBase: 10,
-                    hitstunGrowth: 1,
-                    hitLag: 12,
-                    ignoreOwnerHitlag: false,
-                    // characterSpecific: ,
-                    x: 10,
-                    y: -50,
-                    framesUntilActivation: 0,
-                    duration: 40,
-                    onActivation: (state) => { return state },
-                    onHit: (state) => { return state },
-                    onEnd: (state) => { return state },
-                }
-            ]),
-            ...generateAttack([
-                {
-                    damage: 4,
-                    radius: 20,
-                    knockbackBase: 18,
-                    knockbackGrowth: 2,
-                    knockbackX: 0,
-                    knockbackY: 1,
-                    hitstunBase: 10,
-                    hitstunGrowth: 1,
-                    hitLag: 12,
-                    ignoreOwnerHitlag: false,
-                    // characterSpecific: ,
-                    x: -10,
-                    y: -50,
-                    framesUntilActivation: 0,
-                    duration: 40,
-                    onActivation: (state) => { return state },
-                    onHit: (state) => { return state },
-                    onEnd: (state) => { return state },
-                }
-            ])
-        },
-        airLightDown: {
-            ...generateAttack([
-                {
-                    damage: 10,
-                    radius: 30,
-                    knockbackBase: 30,
-                    knockbackGrowth: 3,
-                    knockbackX: 0,
-                    knockbackY: -1,
-                    hitstunBase: 10,
-                    hitstunGrowth: 1,
-                    hitLag: 12,
-                    ignoreOwnerHitlag: false,
-                    // characterSpecific: ,
-                    x: 0,
-                    y: 50,
-                    framesUntilActivation: 0,
-                    duration: 40,
-                    onActivation: (state) => { return state },
-                    onHit: (state) => { return state },
-                    onEnd: (state) => { return state },
-                }
-            ])
-        },
-        airLightForward: {
-            ...generateAttack([
-                {
-                    damage: 5,
-                    radius: 10,
-                    knockbackBase: 10,
-                    knockbackGrowth: 2,
-                    knockbackX: 1,
-                    knockbackY: 0.2,
-                    hitstunBase: 10,
-                    hitstunGrowth: 1,
-                    hitLag: 12,
-                    ignoreOwnerHitlag: false,
-                    // characterSpecific: ,
-                    x: 15,
-                    y: -5,
-                    framesUntilActivation: 0,
-                    duration: 40,
-                    onActivation: (state) => { return state },
-                    onHit: (state) => { return state },
-                    onEnd: (state) => { return state },
-                }
-            ])
-        },
-        airLightBack: {
-            ...generateAttack([
-                {
-                    damage: 8,
-                    radius: 20,
-                    knockbackBase: 30,
-                    knockbackGrowth: 5,
-                    knockbackX: 1,
-                    knockbackY: 0.4,
-                    hitstunBase: 25,
-                    hitstunGrowth: 1,
-                    hitLag: 12,
-                    ignoreOwnerHitlag: false,
-                    // characterSpecific: ,
-                    x: 30,
-                    y: 0,
-                    framesUntilActivation: 0,
-                    duration: 40,
-                    onActivation: (state) => { return state },
-                    onHit: (state) => { return state },
-                    onEnd: (state) => { return state },
-                }
-            ])
-        },
-        SpecialNeutral: {
-            ...generateAttack([])
-        },
-        SpecialDown: {
-            ...generateAttack([])
-        },
-        airSpecialNeutral: {
-            ...generateAttack([])
-        },
-        airSpecialUp: {
-            ...generateAttack([])
-        },
-        airSpecialDown: {
-            ...generateAttack([])
-        },
-        airSpecialForward: {
-            ...generateAttack([])
-        },
-        airSpecialBack: {
-            ...generateAttack([])
-        },
-        MeterNeutral: {
-            ...generateAttack([])
-        },
-        MeterForward: {
-            ...generateAttack([])
-        },
-        MeterDown: {
-            ...generateAttack([])
-        },
-        airMeterNeutral: {
-            ...generateAttack([])
-        },
-        airMeterUp: {
-            ...generateAttack([])
-        },
-        airMeterDown: {
-            ...generateAttack([])
-        },
-        airMeterForward: {
-            ...generateAttack([])
-        },
-        airMeterBack: {
-            ...generateAttack([])
-        }
+      // If distance increased, gain meter in relation to the change in the sum of x-differences, divided with the number of players
+      const meterGain = Math.max(0, (newXDiff - xDiff) / otherPlayers.length * METER_GAIN_MULTIPLIER)
+      xDiff = newXDiff
+      return gainMeter(meterGain, player)
+    }
+  })(),
+  onMove: (player: Player) => player,
+  onJump: (player: Player) => player,
+  onAttackHit: (player: Player) => player,
+  onGetHit: (player: Player) => player,
+  attacks: {
+    LightNeutral: {
+      ...generateAttack([
+        { ...createHitbox(4, 12, 5), radius: 123 },
+        { ...createHitbox(12, 20, 10), hitLag: 100 }
+      ]),
+      endWhenHitboxConnects: false
     },
+    LightForward: {
+      ...generateAttack([
+        {
+          damage: 4,
+          radius: 20,
+          knockbackBase: 6,
+          knockbackGrowth: 2,
+          knockbackX: 0.5,
+          knockbackY: 1,
+          hitstunBase: 20,
+          hitstunGrowth: 1,
+          hitLag: 12,
+          ignoreOwnerHitlag: false,
+          // characterSpecific: ,
+          x: 5,
+          y: 5,
+          framesUntilActivation: 0,
+          duration: 40,
+          onActivation: (state) => { return state },
+          onHit: (state) => { return state },
+          onEnd: (state) => { return state },
+        }
+      ])
+    },
+    LightDown: {
+      ...generateAttack([
+        {
+          damage: 6,
+          radius: 20,
+          knockbackBase: 15,
+          knockbackGrowth: 5,
+          knockbackX: 1,
+          knockbackY: -1,
+          hitstunBase: 30,
+          hitstunGrowth: 1,
+          hitLag: 12,
+          ignoreOwnerHitlag: false,
+          // characterSpecific: ,
+          x: 30,
+          y: 30,
+          framesUntilActivation: 0,
+          duration: 40,
+          onActivation: (state) => { return state },
+          onHit: (state) => { return state },
+          onEnd: (state) => { return state },
+        }
+      ])
+    },
+    airLightNeutral: {
+      ...generateAttack([
+        {
+          damage: 9,
+          radius: 20,
+          knockbackBase: 1,
+          knockbackGrowth: 1,
+          knockbackX: 1,
+          knockbackY: 1,
+          hitstunBase: 20,
+          hitstunGrowth: 1,
+          hitLag: 12,
+          ignoreOwnerHitlag: false,
+          // characterSpecific: ,
+          x: 0,
+          y: -5,
+          framesUntilActivation: 0,
+          duration: 40,
+          onActivation: (state) => { return state },
+          onHit: (state) => { return state },
+          onEnd: (state) => { return state },
+        }
+      ])
+    },
+    airLightUp: {
+      ...generateAttack([
+        {
+          damage: 4,
+          radius: 20,
+          knockbackBase: 12,
+          knockbackGrowth: 2,
+          knockbackX: 0,
+          knockbackY: 1,
+          hitstunBase: 10,
+          hitstunGrowth: 1,
+          hitLag: 12,
+          ignoreOwnerHitlag: false,
+          // characterSpecific: ,
+          x: 10,
+          y: -50,
+          framesUntilActivation: 0,
+          duration: 40,
+          onActivation: (state) => { return state },
+          onHit: (state) => { return state },
+          onEnd: (state) => { return state },
+        }
+      ]),
+      ...generateAttack([
+        {
+          damage: 4,
+          radius: 20,
+          knockbackBase: 18,
+          knockbackGrowth: 2,
+          knockbackX: 0,
+          knockbackY: 1,
+          hitstunBase: 10,
+          hitstunGrowth: 1,
+          hitLag: 12,
+          ignoreOwnerHitlag: false,
+          // characterSpecific: ,
+          x: -10,
+          y: -50,
+          framesUntilActivation: 0,
+          duration: 40,
+          onActivation: (state) => { return state },
+          onHit: (state) => { return state },
+          onEnd: (state) => { return state },
+        }
+      ])
+    },
+    airLightDown: {
+      ...generateAttack([
+        {
+          damage: 10,
+          radius: 30,
+          knockbackBase: 30,
+          knockbackGrowth: 3,
+          knockbackX: 0,
+          knockbackY: -1,
+          hitstunBase: 10,
+          hitstunGrowth: 1,
+          hitLag: 12,
+          ignoreOwnerHitlag: false,
+          // characterSpecific: ,
+          x: 0,
+          y: 50,
+          framesUntilActivation: 0,
+          duration: 40,
+          onActivation: (state) => { return state },
+          onHit: (state) => { return state },
+          onEnd: (state) => { return state },
+        }
+      ])
+    },
+    airLightForward: {
+      ...generateAttack([
+        {
+          damage: 5,
+          radius: 10,
+          knockbackBase: 10,
+          knockbackGrowth: 2,
+          knockbackX: 1,
+          knockbackY: 0.2,
+          hitstunBase: 10,
+          hitstunGrowth: 1,
+          hitLag: 12,
+          ignoreOwnerHitlag: false,
+          // characterSpecific: ,
+          x: 15,
+          y: -5,
+          framesUntilActivation: 0,
+          duration: 40,
+          onActivation: (state) => { return state },
+          onHit: (state) => { return state },
+          onEnd: (state) => { return state },
+        }
+      ])
+    },
+    airLightBack: {
+      ...generateAttack([
+        {
+          damage: 8,
+          radius: 20,
+          knockbackBase: 30,
+          knockbackGrowth: 5,
+          knockbackX: 1,
+          knockbackY: 0.4,
+          hitstunBase: 25,
+          hitstunGrowth: 1,
+          hitLag: 12,
+          ignoreOwnerHitlag: false,
+          // characterSpecific: ,
+          x: 30,
+          y: 0,
+          framesUntilActivation: 0,
+          duration: 40,
+          onActivation: (state) => { return state },
+          onHit: (state) => { return state },
+          onEnd: (state) => { return state },
+        }
+      ])
+    },
+    SpecialNeutral: {
+      ...generateAttack([])
+    },
+    SpecialDown: {
+      ...generateAttack([])
+    },
+    airSpecialNeutral: {
+      ...generateAttack([])
+    },
+    airSpecialUp: {
+      ...generateAttack([])
+    },
+    airSpecialDown: {
+      ...generateAttack([])
+    },
+    airSpecialForward: {
+      ...generateAttack([])
+    },
+    airSpecialBack: {
+      ...generateAttack([])
+    },
+    MeterNeutral: {
+      ...generateAttack([])
+    },
+    MeterForward: {
+      ...generateAttack([])
+    },
+    MeterDown: {
+      ...generateAttack([])
+    },
+    airMeterNeutral: {
+      ...generateAttack([])
+    },
+    airMeterUp: {
+      ...generateAttack([])
+    },
+    airMeterDown: {
+      ...generateAttack([])
+    },
+    airMeterForward: {
+      ...generateAttack([])
+    },
+    airMeterBack: {
+      ...generateAttack([])
+    }
+  },
 }
--- src/characters/mmkalll.ts
+++ src/characters/mmkalll.ts
@@ -2,160 +2,160 @@ import { Character, Attack, Player } from "../types";
 import { generateAttack, createHitbox, gainMeter, generateProjectile } from "../utilities";
 
 export const mmKALLL: Character = {
-    name: 'mmKALLL',
-    id: 'mmkalll',
-    maxHealth: 100,
-    maxMeter: 100,
-    startingMeter: 0,
-    meterThresholds: [25, 50, 75, 100],
-    walkSpeed: 6.5, // Horizontal speed in pixels per frame
-    airSpeed: 9,
-    weight: 1, // Multiplier relative to mmKALLL
-    maxJumps: 2,
-    jumpStrength: 1, // Multiplier relative to mmKALLL
-    hurtboxRadius: 20, // Sprites are 40x100
-    onEachFrame: (player: Player) => player,
-    onMove: (player: Player) => gainMeter(0.1, player),
-    onJump: (player: Player) => player,
-    onAttackHit: (player: Player) => gainMeter(8, player),
-    onGetHit: (player: Player) => gainMeter(-5, player),
-    attacks: {
-        LightNeutral: {
-            ...generateAttack([
-                { ...createHitbox(4, 3, 3) },
-                { ...createHitbox(12, 8, 6) }
-            ]),
-            duration: 22, // You can override any property of generateAttack by adding it after the ... spread
-        },
-        LightForward: {
-            ...generateAttack([
-                { ...createHitbox(20, 10, 13) }
-            ]),
-            duration: 35,
-        },
-        LightDown: {
-            ...generateAttack([
-                { ...createHitbox(9, 6, 5), radius: 10, x: 80 } // Override also works on hitboxes
-            ])
-        },
-        airLightNeutral: {
-            ...generateAttack([
-                { ...createHitbox(9, 7, 6) },
-                { ...createHitbox(45, 15, 11) },
-                { ...createHitbox(90, 20, 3), radius: 70 }
-            ])
-        },
-        airLightUp: {
-            ...generateAttack([
-                { ...createHitbox(11, 20, 14), y: -50 } // Negative y means upwards
-            ])
-        },
-        airLightDown: {
-            ...generateAttack([
-                { ...createHitbox(30, 20, 22) }
-            ])
-        },
-        airLightForward: {
-            ...generateAttack([
-                { ...createHitbox(4, 40, 5), radius: 30 }
-            ])
-        },
-        // An attack can also have many hitboxes with different timings
-        airLightBack: {
-            ...generateAttack([
-                { ...createHitbox(4, 10, 6), radius: 15,   x: -10 },
-                { ...createHitbox(11, 10, 6), radius: 15,  x: -30 },
-                { ...createHitbox(18, 10, 6), radius: 15,  x: -50 },
-                { ...createHitbox(25, 10, 6), radius: 15,  x: -70 },
-                { ...createHitbox(34, 10, 6), radius: 15,  x: -90 },
-                { ...createHitbox(45, 10, 6), radius: 15,  x: -10 },
-                { ...createHitbox(51, 10, 6), radius: 15,  x: -30 },
-                { ...createHitbox(67, 10, 6), radius: 15,  x: -50 },
-                { ...createHitbox(74, 10, 6), radius: 15,  x: -70 },
-                { ...createHitbox(80, 10, 6), radius: 15, x: -90 },
-                { ...createHitbox(90, 10, 6), radius: 15, x: -10 },
-                { ...createHitbox(100, 10, 6), radius: 15, x: -30 },
-                { ...createHitbox(110, 10, 6), radius: 15, x: -50 },
-                { ...createHitbox(120, 10, 6), radius: 15, x: -70 },
-                { ...createHitbox(130, 10, 6), radius: 15, x: -90 },
-            ]),
-            duration: 40
-        },
+  name: 'mmKALLL',
+  id: 'mmkalll',
+  maxHealth: 100,
+  maxMeter: 100,
+  startingMeter: 0,
+  meterThresholds: [25, 50, 75, 100],
+  walkSpeed: 6.5, // Horizontal speed in pixels per frame
+  airSpeed: 9,
+  weight: 1, // Multiplier relative to mmKALLL
+  maxJumps: 2,
+  jumpStrength: 1, // Multiplier relative to mmKALLL
+  hurtboxRadius: 20, // Sprites are 40x100
+  onEachFrame: (player: Player) => player,
+  onMove: (player: Player) => gainMeter(0.1, player),
+  onJump: (player: Player) => player,
+  onAttackHit: (player: Player) => gainMeter(8, player),
+  onGetHit: (player: Player) => gainMeter(-5, player),
+  attacks: {
+    LightNeutral: {
+      ...generateAttack([
+        { ...createHitbox(4, 3, 3) },
+        { ...createHitbox(12, 8, 6) }
+      ]),
+      duration: 22, // You can override any property of generateAttack by adding it after the ... spread
+    },
+    LightForward: {
+      ...generateAttack([
+        { ...createHitbox(20, 10, 13) }
+      ]),
+      duration: 35,
+    },
+    LightDown: {
+      ...generateAttack([
+        { ...createHitbox(9, 6, 5), radius: 10, x: 80 } // Override also works on hitboxes
+      ])
+    },
+    airLightNeutral: {
+      ...generateAttack([
+        { ...createHitbox(9, 7, 6) },
+        { ...createHitbox(45, 15, 11) },
+        { ...createHitbox(90, 20, 3), radius: 70 }
+      ])
+    },
+    airLightUp: {
+      ...generateAttack([
+        { ...createHitbox(11, 20, 14), y: -50 } // Negative y means upwards
+      ])
+    },
+    airLightDown: {
+      ...generateAttack([
+        { ...createHitbox(30, 20, 22) }
+      ])
+    },
+    airLightForward: {
+      ...generateAttack([
+        { ...createHitbox(4, 40, 5), radius: 30 }
+      ])
+    },
+    // An attack can also have many hitboxes with different timings
+    airLightBack: {
+      ...generateAttack([
+        { ...createHitbox(4, 10, 6), radius: 15,   x: -10 },
+        { ...createHitbox(11, 10, 6), radius: 15,  x: -30 },
+        { ...createHitbox(18, 10, 6), radius: 15,  x: -50 },
+        { ...createHitbox(25, 10, 6), radius: 15,  x: -70 },
+        { ...createHitbox(34, 10, 6), radius: 15,  x: -90 },
+        { ...createHitbox(45, 10, 6), radius: 15,  x: -10 },
+        { ...createHitbox(51, 10, 6), radius: 15,  x: -30 },
+        { ...createHitbox(67, 10, 6), radius: 15,  x: -50 },
+        { ...createHitbox(74, 10, 6), radius: 15,  x: -70 },
+        { ...createHitbox(80, 10, 6), radius: 15, x: -90 },
+        { ...createHitbox(90, 10, 6), radius: 15, x: -10 },
+        { ...createHitbox(100, 10, 6), radius: 15, x: -30 },
+        { ...createHitbox(110, 10, 6), radius: 15, x: -50 },
+        { ...createHitbox(120, 10, 6), radius: 15, x: -70 },
+        { ...createHitbox(130, 10, 6), radius: 15, x: -90 },
+      ]),
+      duration: 40
+    },
 
-        // Special and meter moves are assigned below
-    }
+    // Special and meter moves are assigned below
+  }
 }
 
 // Create special moves:
 
 export const heal: Attack = {
-    ...generateAttack([]),
-    duration: 80,
-    endWhenHitboxConnects: false,
-    endWhenHitboxesEnded: false,
-    endAfterDurationEnded: true,
-    meterCost: 25,
-    onStart: (state, attack) => {
-        state.players[attack.playerSlot].hitlagRemaining = 60
-        return state
-    },
-    onEnd: (state, attack) => {
-        const player = state.players[attack.playerSlot]
-        state.players[attack.playerSlot].health = Math.min(player.health + 10, player.character.maxHealth)
-        return state
-    }
+  ...generateAttack([]),
+  duration: 80,
+  endWhenHitboxConnects: false,
+  endWhenHitboxesEnded: false,
+  endAfterDurationEnded: true,
+  meterCost: 25,
+  onStart: (state, attack) => {
+    state.players[attack.playerSlot].hitlagRemaining = 60
+    return state
+  },
+  onEnd: (state, attack) => {
+    const player = state.players[attack.playerSlot]
+    state.players[attack.playerSlot].health = Math.min(player.health + 10, player.character.maxHealth)
+    return state
+  }
 }
 
 export const hadoken: Attack = {
-    ...generateProjectile([
-        { ...createHitbox(30, 180, 8) }
-    ]),
-    onStart: (state, attack) => {
-        state.players[attack.playerSlot].hitlagRemaining = 30
-        return state
-    },
-    x: -55,
-    meterCost: 10
+  ...generateProjectile([
+    { ...createHitbox(30, 180, 8) }
+  ]),
+  onStart: (state, attack) => {
+    state.players[attack.playerSlot].hitlagRemaining = 30
+    return state
+  },
+  x: -55,
+  meterCost: 10
 }
 
 // Assign them:
 
 mmKALLL.attacks = {
-    ...mmKALLL.attacks,
+  ...mmKALLL.attacks,
 
-    // Special moves
-    SpecialNeutral: {
-        ...generateAttack([])
-    },
-    SpecialForward: {
-        ...generateAttack([])
-    },
-    SpecialDown: {
-        ...generateAttack([])
-    },
-    airSpecialNeutral: {
-        ...generateAttack([])
-    },
-    airSpecialUp: {
-        ...generateAttack([])
-    },
-    airSpecialDown: {
-        ...generateAttack([])
-    },
-    airSpecialForward: {
-        ...generateAttack([])
-    },
-    airSpecialBack: {
-        ...generateAttack([])
-    },
+  // Special moves
+  SpecialNeutral: {
+    ...generateAttack([])
+  },
+  SpecialForward: {
+    ...generateAttack([])
+  },
+  SpecialDown: {
+    ...generateAttack([])
+  },
+  airSpecialNeutral: {
+    ...generateAttack([])
+  },
+  airSpecialUp: {
+    ...generateAttack([])
+  },
+  airSpecialDown: {
+    ...generateAttack([])
+  },
+  airSpecialForward: {
+    ...generateAttack([])
+  },
+  airSpecialBack: {
+    ...generateAttack([])
+  },
 
-    // Meter moves
-    MeterNeutral: heal,
-    MeterForward: hadoken,
-    MeterDown: heal,
-    airMeterNeutral: heal,
-    airMeterUp: heal,
-    airMeterDown: heal,
-    airMeterForward: hadoken,
-    airMeterBack: { ...hadoken, xSpeed: -2.5, x: 5 } // TODO: x: 55 would make sense but makes the projectile spawn too much to the right
+  // Meter moves
+  MeterNeutral: heal,
+  MeterForward: hadoken,
+  MeterDown: heal,
+  airMeterNeutral: heal,
+  airMeterUp: heal,
+  airMeterDown: heal,
+  airMeterForward: hadoken,
+  airMeterBack: { ...hadoken, xSpeed: -2.5, x: 5 } // TODO: x: 55 would make sense but makes the projectile spawn too much to the right
 }
